7a901ca9b5d7b31fcd64bf5f5103163a1691a05a
vini <viniasm13@gmail.com>
2025-11-21 23:24:01 -0300

    Add user prompt and validation for driving license

---
+++ desafios-alura/desafio-02.js
@@ -0,0 +1,11 @@
+// Desafio 02: Prompt e Validação
+// Pergunta o nome do usuário
+let nome = prompt("Qual é o seu nome?");
+
+// Pergunta a idade do usuário
+let idade = prompt("Qual é a sua idade?");
+
+// Valida se pode tirar habilitação
+if (idade >= 18) {
+    alert("Pode tirar a habilitação!");
+}

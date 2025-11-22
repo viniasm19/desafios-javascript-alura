e9ced486bc08710db4c051e6d146a7ac5bf4b2e9
vini <viniasm13@gmail.com>
2025-11-21 23:25:54 -0300

    Add loop examples for counting up and down

---
+++ desafios-alura/desafio-04.js
@@ -0,0 +1,14 @@
+// Desafio 04: Loops While
+// Contador de 1 a 10
+let numero = 1; 
+while (numero <= 10) {
+    alert(numero);
+    numero++;
+}
+
+// Contador de 10 a 0
+let numeroRegressivo = 10; 
+while (numeroRegressivo >= 0) {
+    alert(numeroRegressivo);
+    numeroRegressivo--;
+}
